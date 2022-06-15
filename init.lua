e8dbda3543750787117524cfd4dae5695884f69c
Joshua Rubin <jrubin@groq.com>
2022-06-14 22:24:21 -0700

    hls
    Signed-off-by: Joshua Rubin <jrubin@groq.com>

--- init.lua
+++ init.lua
@@ -768,7 +768,7 @@ end
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 
-local servers = { "bashls", "clangd", "cmake", "dockerls", "pyright", "vimls", "zls" }
+local servers = { "bashls", "clangd", "cmake", "dockerls", "hls", "pyright", "vimls", "zls" }
 for _, lsp in ipairs(servers) do
 	nvim_lsp[lsp].setup({
 		on_attach = on_attach,
