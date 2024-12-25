d3c17d4740342dc2fcb42849eaf63d3bfd20f913
Joshua Rubin <me@jawa.dev>
2024-12-25 01:49:32 -0700

    restore blink cmdline sources back to default
    Signed-off-by: Joshua Rubin <me@jawa.dev>

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -13,6 +13,19 @@ return {
 
 			table.insert(opts.sources.default, "luasnp")
 
+			opts.sources.cmdline = function()
+				local type = vim.fn.getcmdtype()
+				-- Search forward and backward
+				if type == "/" or type == "?" then
+					return { "buffer" }
+				end
+				-- Commands
+				if type == ":" then
+					return { "cmdline" }
+				end
+				return {}
+			end
+
 			opts.completion.accept.auto_brackets.enabled = false
 			opts.completion.list = {
 				selection = "manual",
