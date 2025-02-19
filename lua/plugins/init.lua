161634dde04ee30fd5638a7fac29cf5e11bf7554
Joshua Rubin <me@jawa.dev>
2025-02-19 15:55:39 -0700

    add flatten.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -124,4 +124,65 @@ return {
 			},
 		},
 	},
+	{
+		"willothy/flatten.nvim",
+		-- Ensure that it runs first to minimize delay when opening file from terminal
+		lazy = false,
+		priority = 1001,
+		opts = function()
+			local saved_terminal
+			return {
+				nest_if_no_args = true, -- fix jj split
+				window = {
+					-- this is the same as "vsplit" with focus == "first", however
+					-- if the calling buffer is a terminal, the vsplit is made
+					-- relative to the previous window
+					open = function(opts)
+						local focus = opts.files[1]
+						local win = 0
+						if vim.bo.buftype == "terminal" then
+							-- use previous window
+							win = vim.fn.win_getid(vim.fn.winnr("#"))
+						end
+						local winnr = vim.api.nvim_open_win(focus.bufnr, true, {
+							vertical = true,
+							win = win,
+						})
+						return focus.bufnr, winnr
+					end,
+				},
+				hooks = {
+					pre_open = function(opts)
+						local win = vim.api.nvim_get_current_win()
+						local buf = vim.api.nvim_win_get_buf(win)
+
+						for _, term in ipairs(Snacks.terminal.list()) do
+							if term.closed == false and term.buf == buf and term.win == win then
+								saved_terminal = term
+								break
+							end
+						end
+						return require("flatten").hooks.pre_open(opts)
+					end,
+					post_open = function(opts)
+						if opts.is_blocking and saved_terminal then
+							-- hide the terminal while it's blocking
+							saved_terminal:toggle()
+						end
+						return require("flatten").hooks.post_open(opts)
+					end,
+					block_end = function(opts)
+						-- after blocking ends (for a git commit, etc), reopen the terminal
+						vim.schedule(function()
+							if saved_terminal then
+								saved_terminal:toggle()
+								saved_terminal = nil
+							end
+						end)
+						return require("flatten").hooks.block_end(opts)
+					end,
+				},
+			}
+		end,
+	},
 }
