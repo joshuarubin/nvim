ad6c506825c49edc336b7838a37d7ef8a3d0b804
Joshua Rubin <me@jawa.dev>
2025-11-25 20:47:20 +0200

    latest updates

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -3,5 +3,13 @@ return {
 		"MeanderingProgrammer/render-markdown.nvim",
 		ft = { "markdown", "norg", "rmd", "org", "Avante", "codecompanion" },
 		cond = not vim.g.vscode,
+		opts = {
+			win_options = {
+				conceallevel = {
+					default = 3,
+					rendered = 3,
+				},
+			},
+		},
 	},
 }
